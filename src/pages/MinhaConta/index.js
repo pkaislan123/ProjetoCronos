f86ac30b14db2c0426b0e0e7eeac82282650c512
pkaislan123 <comprasaislan@gmail.com>
2023-11-27 19:37:27 -0300

    commit de pastas

--- src/pages/MinhaConta/index.js
+++ src/pages/MinhaConta/index.js
@@ -9,23 +9,40 @@ import Skeleton from '@material-ui/lab/Skeleton';
 import Button from '@material-ui/core/Button';
 
 import { useHistory } from 'react-router-dom';
-import TableBody from '@material-ui/core/TableBody';
 import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import { styled } from '@material-ui/styles';
+
 import './styles.scss';
 import Cookies from 'js-cookie';
-
+import { makeStyles } from '@material-ui/core/styles';
 import api from '../../services/api';
 import moment from 'moment';
 
 
 
+import { styled } from '@material-ui/styles';
 
+
+import {
+    DataGridPro,
+    GridToolbarContainer,
+    GridToolbarColumnsButton,
+    GridToolbarFilterButton,
+    GridToolbarExport,
+    GridToolbarDensitySelector,
+} from '@mui/x-data-grid-pro';
+
+import {  ptBR } from '@material-ui/data-grid';
+
+const ColorButtonRed = styled(Button)(({ theme }) => ({
+    color: 'white',
+    backgroundColor: 'red',
+    size: 'large',
+    borderColor: 'black',
+    '&:hover': {
+        backgroundColor: 'red',
+        color: 'white',
+    },
+}));
 
 export const MuiSwitchLarge = styled(Switch)(({ theme }) => ({
     width: 80,
@@ -56,18 +73,26 @@ export const MuiSwitchLarge = styled(Switch)(({ theme }) => ({
 export default function MinhaConta() {
 
 
+
+
+
+
     const [loadingCronometros, setLoadingCronometros] = useState(true);
     const [cronometros, setCronometros] = useState(null);
     const history = useHistory();
+  
+		
 
     const [somaMilissegundosArrancadas, setSomaMilissegundosArrancadas] = useState(0);
     const [somaMilissegundosPassagens, setSomaMilissegundosPassagens] = useState(0);
+   const [somaMilissegundosArrancadasBot, setSomaMilissegundosArrancadasBot] = useState(0);
     const [totalArrancadas, setTotalArracadas] = useState(0);
     const [totalPassagens, setTotalPassagens] = useState(0);
+  const [totalArrancadasBot, setTotalArracadasBot] = useState(0);
 
     const [melhorTempoArrancada, setMelhorTempoArrancada] = useState(0);
     const [melhorTempoPassagem, setMelhorTempoPassagem] = useState(0);
-
+  const [melhorTempoArrancadaBot, setMelhorTempoArrancadaBot] = useState(0);
 
     const [value, setValue] = React.useState(0);
 
@@ -95,9 +120,32 @@ export default function MinhaConta() {
 
         listarCronometros();
 
+
+
+
     }, []);
 
+const confirmarExclusao = (idCronometro) => {
+  if (window.confirm('Tem certeza que deseja excluir este cronômetro?')) {
+    excluirCronometro(idCronometro);
+  }
+};
 
+
+const excluirCronometro = async (idCronometro) => {
+ 
+
+  try {
+    const url = `v1/cronometros/excluirporid/${idCronometro}`;
+    const response = await api.delete(url);
+
+    if (response.data !== null) {
+      listarCronometros();
+    }
+  } catch (err) {
+    alert('Erro de Conexão!');
+  }
+};
 
 
     async function listarCronometros() {
@@ -108,7 +156,7 @@ export default function MinhaConta() {
         try {
 
             // console.log("Url: " + url);
-            var url = "v1/cronometros/listarPorCiclista/" + Cookies.get('id_ciclista');
+            var url = "v1/cronometros/listarPorCiclistaDesc/" + Cookies.get('id_ciclista');
             // console.log("Url: " + url);
             await api.get(url).then(function (response) {
 
@@ -124,7 +172,7 @@ export default function MinhaConta() {
                                 accumulator.countTipoCronometroArrancadas++;
 
                                 // Verifica se o valor atual é menor do que o menor valor armazenado
-                                if (cronometro.millissegundos < accumulator.menorArrancada  || accumulator.menorArrancada === undefined) {
+                                if (cronometro.millissegundos < accumulator.menorArrancada || accumulator.menorArrancada === undefined) {
                                     accumulator.menorArrancada = cronometro.millissegundos;
                                 }
                             }
@@ -163,9 +211,38 @@ export default function MinhaConta() {
                     setMelhorTempoPassagem(menorPassagem);
 
 
+     // Calcula a soma dos milissegundos bot
+                    const { totalMilissegundosArrancadaBot, countTipoCronometroArrancadasBot, menorArrancadaBot } = response.data.reduce(
+                        (accumulator, cronometro) => {
+                            if (cronometro.tipo_cronometro === 2) {
+                                accumulator.totalMilissegundosArrancadaBot += cronometro.millissegundos;
+                                accumulator.countTipoCronometroArrancadasBot++;
+
+                                // Verifica se o valor atual é menor do que o menor valor armazenado
+                                if (cronometro.millissegundos < accumulator.menorArrancadaBot || accumulator.menorArrancadaBot === undefined) {
+                                    accumulator.menorArrancadaBot = cronometro.millissegundos;
+                                }
+                            }
+                            return accumulator;
+                        },
+                        { totalMilissegundosArrancadaBot: 0, countTipoCronometroArrancadasBot: 0, menorArrancadaBot: undefined }
+                    );
+
+
+                    console.log('Soma dos milissegundos:', totalMilissegundosArrancadaBot);
+                    console.log('Número de objetos com tipo_cronometro igual a 2:', countTipoCronometroArrancadasBot);
+
+
+                    setSomaMilissegundosArrancadasBot(totalMilissegundosArrancadaBot);
+                    setTotalArracadasBot(countTipoCronometroArrancadasBot);
+                    setMelhorTempoArrancadaBot(menorArrancadaBot);
+
+
+
+
 
                     setLoadingCronometros(false);
-
+         
                 }
 
 
@@ -186,47 +263,7 @@ export default function MinhaConta() {
     }
 
 
-    function RowCronometro(props) {
-        const { row } = props;
-
-
-        return (
-            <React.Fragment>
-                <TableRow sx={{ '& > *': { borderBottom: 'unset' } }} style={{ fontFamily: 'A4 SPEED FONT', textAlign: "center", fontSize: 14, color: 'white', backgroundColor: 'black' }}>
-
-                    <TableCell colSpan={1} align="center">
-                        <Typography variant="h6" component="h4" style={{ fontFamily: 'A4 SPEED FONT', color: 'white' }}>
-                            {row.id_cronometro}
-                        </Typography>
-
-                    </TableCell>
-
-                    <TableCell colSpan={1} align="center">
-                        <Typography variant="h6" component="h4" style={{ fontFamily: 'A4 SPEED FONT', color: 'white' }}>
-                            {row.tipo_cronometro === 0 ? "Arrancada" : "Passagem"}
-                        </Typography>
-                    </TableCell>
-
-                    <TableCell colSpan={1} align="center">
-                        <Typography variant="h6" component="h4" style={{ fontFamily: 'A4 SPEED FONT', color: 'white' }}>
-                            {formatDataEHour(row.data_hora_salvamento)}
-                        </Typography>
-                    </TableCell>
-
-
-                    <TableCell colSpan={1} align="center">
-                        <Typography variant="h6" component="h4" style={{ fontFamily: 'A4 SPEED FONT', color: 'white' }}>
-                            {MilissegundosParaTempo(row.millissegundos)}
-                        </Typography>
-                    </TableCell>
-
-                </TableRow>
-            </React.Fragment>
-        );
-    }
-
-
-
+   
 
     function sair() {
 
@@ -238,7 +275,118 @@ export default function MinhaConta() {
 
     }
 
+    function CustomToolbar() {
+        return (
+            <GridToolbarContainer>
+                <GridToolbarColumnsButton />
+                <GridToolbarFilterButton />
+                <GridToolbarDensitySelector />
+                <GridToolbarExport />
+            </GridToolbarContainer>
+        );
+    }
 
+
+    const columnsDataGrid = [
+
+
+        {
+            headerName: 'ID',
+            field: 'id_cronometro',
+            id: 1,
+            headerClassName: 'cabecalho_amarelo',
+            hide: 'sm'
+        },
+        {
+            headerName: 'Modo',
+            field: 'tipo_cronometro',
+            minWidth: 300,
+            id: 2,
+            headerClassName: 'cabecalho_amarelo',
+            valueFormatter: (params) => {
+                const valueFormatted = params.value === 0 ? "LARGADA BIPE" : params.value === 1 ? "PASSAGEM" : "LARGADA BOT";
+                return `${valueFormatted}`;
+              },
+
+        },
+        {
+            headerName: 'Data',
+            field: 'data_hora_salvamento',
+            minWidth: 300,
+            id: 3,
+ hide: 'sm',
+            headerClassName: 'cabecalho_amarelo',
+            valueFormatter: (params) => {
+                const valueFormatted = formatDataEHour(params.value);
+                return `${valueFormatted}`;
+              },
+ sortComparator: (v1, v2, cellParams1, cellParams2) => {
+    // Implemente sua lógica de comparação de datas aqui
+    const date1 = new Date(v1);
+    const date2 = new Date(v2);
+    
+    if (date1 < date2) {
+      return -1;
+    } else if (date1 > date2) {
+      return 1;
+    } else {
+      return 0;
+    }
+  },
+
+        },
+
+        {
+            field: 'millissegundos',
+            headerName: 'Tempo',
+            minWidth: 100,
+            headerClassName: 'cabecalho_amarelo',
+            id: 4,
+            valueFormatter: (params) => {
+                const valueFormatted = MilissegundosParaTempo(params.value);
+                return `${valueFormatted}`;
+              },
+
+        },
+
+{
+    headerName: 'Ações',
+    
+    headerClassName: 'cabecalho_amarelo',
+    renderCell: (params) => (
+      <ColorButtonRed
+        variant="contained"
+        color="error"
+        onClick={() => confirmarExclusao(params.row.id_cronometro)}
+      >
+        Excluir
+      </ColorButtonRed>
+    ),
+  },
+
+
+    ];
+
+
+
+
+  const handleCellClick = (param, event) => {
+    event.stopPropagation();
+  };
+
+  const handleRowClick = (param, event) => {
+    event.stopPropagation();
+  };
+
+  const onPageChange = (page, pageSize) => {
+    setCronometros(cronometros.slice((page - 1) * pageSize, page * pageSize));
+  };
+
+  const getRowId = (row) => row.id_cronometro;
+
+
+
+  
 
     return (
 
@@ -264,9 +412,6 @@ export default function MinhaConta() {
                                 justifyContent={"center"}
                             >
 
-
-
-
                                 <Grid item xs={12}
                                     container
                                     alignItems={"center"}
@@ -286,7 +431,7 @@ export default function MinhaConta() {
 
                                         <Grid xs={12}
                                         >
-                                            <Typography style={{ fontSize: '14', fontFamily: 'A4 SPEED FONT' }} variant="h5">{totalArrancadas} arrancadas</Typography>
+                                            <Typography style={{ fontSize: '14', fontFamily: 'A4 SPEED FONT' }} variant="h5">{totalArrancadas} arrancadas Bipe</Typography>
                                         </Grid>
 
                                         <Grid xs={1}
@@ -375,6 +520,55 @@ export default function MinhaConta() {
                                     </Grid>
 
                                 </Grid>
+
+  <Grid
+                                        item xs={12} sm={12} md={12} lg={5} xl={5}
+                                        container
+                                        direction='row'
+                                        alignItems={"center"}
+                                        justifyContent={"center"}
+                                        component={Paper} elevation={6} className={"Paper"}
+                                        style={{ backgroundColor: 'purple', color: 'white', margin: 5, padding: 50, marginTop: 20, paddingBottom: 20, borderRadius: '10px' }}
+
+                                    >
+
+                                        <Grid xs={12}
+                                        >
+                                            <Typography style={{ fontSize: '14', fontFamily: 'A4 SPEED FONT' }} variant="h5">{totalArrancadasBot} arrancadas Botao</Typography>
+                                        </Grid>
+
+                                        <Grid xs={1}
+                                            style={{ marginLeft: 8 }}
+                                        >
+                                            <img src="https://cdn-icons-png.flaticon.com/512/1021/1021194.png" alt="Volume Coletado" />
+
+                                        </Grid>
+
+                                        <Grid xs={8}
+                                            style={{ paddingLeft: 10 }}
+                                        >
+                                            <Typography style={{ fontFamily: 'A4 SPEED FONT', marginTop: '0' }} variant="h2">{MilissegundosParaTempo(melhorTempoArrancadaBot)}</Typography>
+                                        </Grid>
+
+                                        <Grid xs={2}
+                                        >
+                                        </Grid>
+
+
+                                        <Grid xs={1}
+                                        >
+                                            <img src="https://cdn-icons-png.flaticon.com/512/4474/4474370.png" alt="Volume Coletado" />
+
+                                        </Grid>
+
+                                        <Grid xs={11}
+                                            style={{ paddingLeft: 10 }}
+                                        >
+                                            <p style={{ margin: '0', fontSize: '5', fontFamily: 'A4 SPEED FONT' }}>total: </p>
+                                            <Typography style={{ fontFamily: 'A4 SPEED FONT', marginTop: '0' }} variant="h5">{MilissegundosParaTempo(somaMilissegundosArrancadasBot)}</Typography>
+                                        </Grid>
+
+                                    </Grid>
 
 
                             </Grid>
@@ -466,30 +660,49 @@ export default function MinhaConta() {
                                                 container
                                                 direction="row"
                                                 alignItems="center"
-                                                style={{ backgroundColor: 'yellow' }}
+                                                style={{ backgroundColor: 'yellow', color:'black' }}
                                             >
 
-                                                <TableContainer component={Paper} style={{ backgroundColor: 'white' }}>
-                                                    <Table aria-label="collapsible table">
-                                                        <TableHead style={{ backgroundColor: 'white' }}>
-                                                            <TableRow style={{ fontSize: 8 }}>
-                                                                <TableCell style={{ fontFamily: 'A4 SPEED FONT', backgroundColor: 'yellow', color: 'black', position: "sticky", top: 0, textAlign: "center" }} colSpan={1}>ID</TableCell>
-                                                                <TableCell style={{ fontFamily: 'A4 SPEED FONT', backgroundColor: 'yellow', color: 'black', position: "sticky", top: 0, textAlign: "center" }} colSpan={1}>Modo</TableCell>
-                                                                <TableCell style={{ fontFamily: 'A4 SPEED FONT', backgroundColor: 'yellow', color: 'black', position: "sticky", top: 0, textAlign: "center" }} colSpan={1}>Data</TableCell>
-                                                                <TableCell style={{ fontFamily: 'A4 SPEED FONT', backgroundColor: 'yellow', color: 'black', position: "sticky", top: 0, textAlign: "center" }} colSpan={1}>Cronômetro</TableCell>
+                                                <div style={{ height: '80%' }}>
+                                                    <div style={{ width: '100%', height: '100%' }} >
+                                                    <DataGridPro localeText={ptBR.props.MuiDataGrid.localeText}
+                                                       
+                                                            getRowId={getRowId}
+                                                            rows={cronometros} columns={columnsDataGrid} disableColumnResize={false}
+                                                            onCellClick={handleCellClick}
+                                                            onRowClick={handleRowClick}
+                                                            components={{
+                                                                Toolbar: CustomToolbar,
+                                                             }}
 
+                                                            pagination
+                                                            page={0}
+                                                            pageSize={25}
 
-                                                            </TableRow>
-                                                        </TableHead>
-                                                        <TableBody>
-                                                            {cronometros.map((cronometro) => (
-                                                                <RowCronometro key={cronometro.id_cronometro} row={cronometro} />
-                                                            ))}
+                                                            rowsPerPageOptions={[]}
+                                                            onPageChange={onPageChange}
+                                                          getRowClassName={(params) => {
+  
+   
+   return  'logs';
+}}
+sx={{
+  "& .MuiDataGrid-row:hover": {
+    backgroundColor: "yellow",
+    color: "black"
+  },
 
+  "& .MuiDataGrid-row:isSelected": {
+    backgroundColor: "green",
+    // color: "red"
+  },
+}}
+                                                           
+                                                        />
+                                                     
+                                                    </div>
+                                                </div>
 
-                                                        </TableBody>
-                                                    </Table>
-                                                </TableContainer >
                                             </Grid>
                                         </Grid>
                                 }
